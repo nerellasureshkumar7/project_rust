b484f3fdc69a2f85b29c13a8abcbfb76e63eb1d4
nerellasureshkumar7 <nerellasureshkumar7@gmail.com>
2024-02-23 20:21:19 +0530

    Added cargo sample project

---
+++ hello_cargo/target/debug/deps/hello_cargo-11f0ab296315d018.d
@@ -0,0 +1,5 @@
+/home/suresh/project_rust/hello_cargo/target/debug/deps/hello_cargo-11f0ab296315d018: src/main.rs
+
+/home/suresh/project_rust/hello_cargo/target/debug/deps/hello_cargo-11f0ab296315d018.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/suresh/project_rust/hello_cargo/target/debug/hello_cargo: /home/suresh/project_rust/hello_cargo/src/main.rs
